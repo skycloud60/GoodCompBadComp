5bc35dbe615c1f029bae69cbd6a0a6908610e134
RvBVakama <RvBVakama@users.noreply.github.com>
2018-01-27 01:39:42 +1100

    Camera follows the player without rolling with the player

---
+++ Tag Mission/Assets/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // The player gameobject accessor
+    public GameObject goPlayer;
+
+    // The offset vector to keep the camera following the player
+    private Vector3 v3Offset;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Set the offset to be the current camera position away from the player 
+        v3Offset = transform.position - goPlayer.transform.position;
+    }
+
+    // LateUpdate is called every frame, if the Behaviour is enabled
+    void LateUpdate()
+    {
+        // Move the camera with the player
+        transform.position = goPlayer.transform.position + v3Offset;
+    }
+}
---
+++ Tag Mission/Assets/Scripts/TheVirus.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TheVirus : MonoBehaviour
+{
+    // Health
+    public float fHealth = 0.0f;
+
+    // Infected or not
+    public bool bInfected = false;
+
+    // How fast the player loses health when infected
+    public float fSpeedOfHPDeduction = 0.0f;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If the player is infected deduct health
+        if (bInfected)
+        {
+            fHealth -= 1.0f * (Time.deltaTime * fSpeedOfHPDeduction);
+        }
+
+        // If the player is dead kill them
+        if (fHealth < 0.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        // Player infection logic
+        if (col.gameObject.tag == "Player")
+        {
+            // If the player was touched and they are infected
+            if (col.gameObject.GetComponent<TheVirus>().bInfected)
+            {
+                // Enemy become infected
+                bInfected = true;
+
+                // Player loses their infection
+                col.gameObject.GetComponent<TheVirus>().bInfected = false;
+            }
+        }
+
+        // Enemy infection logic
+        if (col.gameObject.tag == "Enemy")
+        {
+            if (col.gameObject.GetComponent<TheVirus>().bInfected)
+            {
+                // Player becomes infected
+                bInfected = true;
+
+                // Enemy loses their infection
+                col.gameObject.GetComponent<TheVirus>().bInfected = false;
+            }
+        }
+    }
+}
