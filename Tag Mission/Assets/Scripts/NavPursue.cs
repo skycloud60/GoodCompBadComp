d3025573ab317d8180377a90a17c37423807dc5b
NerbzHub <nathan.nette@gmail.com>
2018-01-28 14:35:07 +1100

    NavPursue

---
+++ Tag Mission/Assets/Scripts/NavPursue.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class NavPursue : MonoBehaviour
+{
+    public Transform goal;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gameObject.GetComponent<VirusTwo>().bInfected)
+        {
+            NavMeshAgent agent = GetComponent<NavMeshAgent>();
+            agent.destination = goal.position;
+        }
+    }
+}
