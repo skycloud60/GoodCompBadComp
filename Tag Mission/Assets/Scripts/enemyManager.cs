459fe0e47457323807b8d7f08c1d8e1572f642ee
skycloud60 <aless.baiocchi@gmail.com>
2018-01-28 12:56:22 +1100

    LETS HOPE THIS WORKS BOIS

--- Tag Mission/Assets/Scripts/enemyManager.cs
+++ Tag Mission/Assets/Scripts/enemyManager.cs
@@ -8,6 +8,7 @@ public class enemyManager : MonoBehaviour
 
     private Transform trnsSli;
     private Transform trnsEnemy;
+	private Transform trnsMesh;
 
     private bool bLock = true;
 
@@ -17,17 +18,22 @@ public class enemyManager : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        trnsSli = gameObject.transform.GetChild(0);
-        trnsEnemy = gameObject.transform.GetChild(1);
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        trnsSli.position = trnsEnemy.position;
-        Transform trans = trnsSli;
-        trnsSli.position = new Vector3(trans.position.x, fHeightOfHealthBars, trans.position.z);
+		trnsSli = gameObject.transform.GetChild(0);
+		trnsEnemy = gameObject.transform.GetChild(1);
+		trnsMesh = gameObject.transform.GetChild(2);
+	}
+	// Update is called once per frame
+	void Update()
+	{
+		trnsSli.position = trnsEnemy.position;
+		trnsMesh.position = trnsSli.position;
+		Transform trans = trnsSli;
+		trnsSli.position = new Vector3(trans.position.x, fHeightOfHealthBars, trans.position.z);
+		Transform trans2 = trnsMesh;
+		trnsMesh.position = new Vector3(trans2.position.x, 0f, trans2.position.z);
 
-        if (goTarget)
-            trnsSli.LookAt(goTarget.transform);
-    }
+
+		if (goTarget)
+			trnsSli.LookAt(goTarget.transform);
+	}
 }
